8fac09f6b87a124ce79e01658a77fc5fe6c7c684
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-03-27 00:15:23 +0700

    feat: update portfolio

--- frontend/src/components/Main/Portfolio/Portfolio.jsx
+++ frontend/src/components/Main/Portfolio/Portfolio.jsx
@@ -4,37 +4,47 @@ function Portfolio() {
   return (
     <section className="section portfolio">
       <h3 className="portfolio__title">
-        Портфолио
+        Портфолио (ссылки на репозитории)
       </h3>
       <ul className="portfolio__list">
         <li className="portfolio__list-item">
           <a
             className="portfolio__link link-animation"
-            href="https://github.com/aleksdsgn"
+            href="https://github.com/aleksdsgn/movies-explorer-full"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Статичный сайт
+            Одностраничное приложение Movies&#8209;Explorer
           </a>
         </li>
         <li className="portfolio__list-item">
           <a
             className="portfolio__link link-animation"
-            href="https://github.com/aleksdsgn"
+            href="https://github.com/aleksdsgn/react-mesto-api-full"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Адаптивный сайт
+            Одностраничное приложение Mesto
           </a>
         </li>
         <li className="portfolio__list-item">
           <a
             className="portfolio__link link-animation"
-            href="https://github.com/aleksdsgn"
+            href="https://github.com/aleksdsgn/russian-travel"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Одностраничное приложение
+            Адаптивный сайт &laquo;Путешествия по&nbsp;России&raquo;
+          </a>
+        </li>
+        <li className="portfolio__list-item">
+          <a
+            className="portfolio__link link-animation"
+            href="https://github.com/aleksdsgn/how-to-learn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Статичный сайт &laquo;Научиться учиться&raquo;
           </a>
         </li>
       </ul>
